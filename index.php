957166f3aa35898302105a0ce357c631bf62bfbb
ceejayvinoya <49735827+ceejayvinoya@users.noreply.github.com>
2019-04-20 00:36:14 +0800

    Update index.php

--- index.php
+++ index.php
@@ -14,7 +14,7 @@
     table {
 	font: 18px/28px Verdana, Arial, Helvetica, sans-serif;
 	border-collapse: collapse;
-	width: 300px;
+	width: 80%;
 	}
 
     th {
@@ -45,19 +45,19 @@
 </ul>
 </div>
 
-<div style = "position: absolute; top: 60px; width: 75%">
-<iframe width="450" height="260" style="border: 2px solid #00008b;" src="https://thingspeak.com/channels/743613/charts/1?bgcolor=%23ffffff&color=%23d62020&dynamic=true&api_key=C3JY2CR1I5TT22F1&results=60&type=line&update=15"></iframe>
+<div style = "position: relative; top: 60px; width: 75%">
+<iframe width="50%" height="260" style="position: absolute; border: 2px solid #00008b;" src="https://thingspeak.com/channels/743613/charts/1?bgcolor=%23ffffff&color=%23d62020&dynamic=true&api_key=C3JY2CR1I5TT22F1&results=60&type=line&update=15"></iframe>
 
-<iframe width="450" height="260" style="border: 2px solid #00008b;" src="https://thingspeak.com/channels/743613/charts/2?bgcolor=%23ffffff&color=%23d62020&dynamic=true&api_key=C3JY2CR1I5TT22F1&results=60&type=line&update=15"></iframe>
+<iframe width="50%" height="260" style="position: absolute; left: 50%; border: 2px solid #00008b;" src="https://thingspeak.com/channels/743613/charts/2?bgcolor=%23ffffff&color=%23d62020&dynamic=true&api_key=C3JY2CR1I5TT22F1&results=60&type=line&update=15"></iframe>
 </div>
 
-<div style = "position: absolute; top: 330px; width: 75%">
-<iframe width="450" height="260" style="border: 2px solid #00008b;" src="https://thingspeak.com/channels/754899/charts/1?bgcolor=%23ffffff&color=%23d62020&dynamic=true&api_key=GVG0HRXKSNZ1IEFH&results=60&type=line&update=15"></iframe>
+<div style = "position: relative; top: 330px; width: 75%">
+<iframe width="450" height="260" style="position: absolute; border: 2px solid #00008b;" src="https://thingspeak.com/channels/754899/charts/1?bgcolor=%23ffffff&color=%23d62020&dynamic=true&api_key=GVG0HRXKSNZ1IEFH&results=60&type=line&update=15"></iframe>
 
-<iframe width="450" height="260" style="border: 2px solid #00008b;" src="https://thingspeak.com/channels/754899/charts/2?bgcolor=%23ffffff&color=%23d62020&dynamic=true&api_key=GVG0HRXKSNZ1IEFH&results=60&type=line&update=15"></iframe>
+<iframe width="450" height="260" style="position: absolute; left: 50%; border: 2px solid #00008b;" src="https://thingspeak.com/channels/754899/charts/2?bgcolor=%23ffffff&color=%23d62020&dynamic=true&api_key=GVG0HRXKSNZ1IEFH&results=60&type=line&update=15"></iframe>
 </div>
 
-<div style = "position: absolute; top: 60px; left: 75%">
+<div style = "position: relative; top: 60px; left: 75%">
 <table>
   <tr class="blue">
     <td>Hardware</td>
