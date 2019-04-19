513b96993ae7bb90cb839c48a7973e5ea04e03db
ceejayvinoya <49735827+ceejayvinoya@users.noreply.github.com>
2019-04-20 06:25:01 +0800

    Update index.php

--- index.php
+++ index.php
@@ -9,27 +9,7 @@
    <script src="script.js"></script>
    <title>AAPMMS - DLSUD</title>
 </head>
-<style>
-    table {
-	font: 18px/28px Verdana, Arial, Helvetica, sans-serif;
-	border-collapse: collapse;
-	position: absolute;
-	width: 100%;
-	}
-    th {
-	padding: 0 0.5em;
-	text-align: left;
-	}
-    tr.blue td {
-	border: 3px solid #FB7A31;
-	background: #FFC;
-	}
-    td {
-	border: 3px solid #CCC;
-	padding: 0 0.5em;
-    text-align: center;
-	}
-</style>
+
 <body>
 
 <div id='cssmenu'>
@@ -37,63 +17,22 @@
    <li class='active'><a href='#'><span>Home</span></a></li>
    <li><a href='info.php'><span>Incoming Air</span></a></li>
    <li><a href='outfo.php'><span>Outgoing Air</span></a></li>
+   <li><a href='status.php'><span>Status Update</span></a></li>
    <li class='last'><a href='#', onClick="alert('One of the world’s leading problem is air pollution and there are many forms of pollutant that contribute in air pollution. Particulate matter which is the sum of all solid and liquid particles suspended in air many of which are hazardous. This complex mixture includes both organic and inorganic particles, such as dust, pollen, soot, smoke, and liquid droplets.This website monitors the daily value of particulate matter and with the hardware installed along Aguinaldo Highway, users can see progress of the mitigation through this website.')"><span>About</span></a></li>
 </ul>
 </div>
 
-<div style = "position: relative; top: 10px; width: 80%">
+<div style = "position: relative; top: 10px; width: 100%">
 <iframe width="49%" height="260" style="position: absolute; border: 2px solid #00008b;" src="https://thingspeak.com/channels/743613/charts/1?bgcolor=%23ffffff&color=%23d62020&dynamic=true&api_key=C3JY2CR1I5TT22F1&results=60&type=line&update=15"></iframe>
 
 <iframe width="49%" height="260" style="position: absolute; left: 50%; border: 2px solid #00008b;" src="https://thingspeak.com/channels/743613/charts/2?bgcolor=%23ffffff&color=%23d62020&dynamic=true&api_key=C3JY2CR1I5TT22F1&results=60&type=line&update=15"></iframe>
 </div>
 
-<div style = "position: relative; top: 280px; width: 80%">
-<iframe width="49%" height="260" style="position: absolute; border: 2px solid #00008b;" src="https://thingspeak.com/channels/754899/charts/1?bgcolor=%23ffffff&color=%23d62020&dynamic=true&api_key=GVG0HRXKSNZ1IEFH&results=60&type=line&update=15"></iframe>
+<div style = "position: relative; top: 280px; width: 100%">
+<iframe width="49%" height="260" style="display: inline-block; position: absolute; border: 2px solid #00008b;" src="https://thingspeak.com/channels/754899/charts/1?bgcolor=%23ffffff&color=%23d62020&dynamic=true&api_key=GVG0HRXKSNZ1IEFH&results=60&type=line&update=15"></iframe>
 
 <iframe width="49%" height="260" style="position: absolute; left: 50%; border: 2px solid #00008b;" src="https://thingspeak.com/channels/754899/charts/2?bgcolor=%23ffffff&color=%23d62020&dynamic=true&api_key=GVG0HRXKSNZ1IEFH&results=60&type=line&update=15"></iframe>
 </div>
 
-<div style = "position: relative; top: 10px; left: 80%; width: 20%">
-<table>
-  <tr class="blue">
-    <td>Hardware</td>
-    <td>Status</td> 
-  </tr>
-<?php
-    $jsondata = file_get_contents("https://api.thingspeak.com/channels/743613/feeds.json?api_key=R63HB8RHEB1IUOZF&timezone=Asia/Hong_Kong&results=1");
-    $json = json_decode($jsondata, true);
-    $timestamp = $json['feeds'][0]['created_at'];
-    $divider = explode("T",$timestamp);
-    $date = $divider[0];
-    $timeset = $divider[1];
-    $timefix = explode("+",$timeset);
-    $time = $timefix[0];
-    $B = date("Y-m-d h:i:s");
-    $timeA = strtotime($date.' '.$time);
-    $timeB = strtotime($B);
-    if(300 > $timeB - $timeA){
-        echo '<tr><td>Sensor 1</td><td>Running</td></tr>';
-    } else {
-        echo '<tr><td>Sensor 1</td><td>Inactive</td></tr>';
-    }
-     $jsondata = file_get_contents("https://api.thingspeak.com/channels/754899/feeds.json?api_key=PJ2C7CICC344DUVR&timezone=Asia/Hong_Kong&results=1");
-    $json = json_decode($jsondata, true);
-    $timestamp = $json['feeds'][0]['created_at'];
-    $divider = explode("T",$timestamp);
-    $date = $divider[0];
-    $timeset = $divider[1];
-    $timefix = explode("+",$timeset);
-    $time = $timefix[0];
-    $B = date("Y-m-d h:i:s");
-    $timeA = strtotime($date.' '.$time);
-    $timeB = strtotime($B);
-    if(300 > $timeB - $timeA){
-        echo '<tr><td>Sensor 2</td><td>Running</td></tr>';
-    } else {
-        echo '<tr><td>Sensor 2</td><td>Inactive</td></tr>';
-    }    
-?>
-</table>
-</div>
 </body>
 </html>
