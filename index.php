01be28e1b4c49bfe5131cfbd34712717230c7290
ceejayvinoya <49735827+ceejayvinoya@users.noreply.github.com>
2019-04-20 17:59:55 +0800

    Update index.php

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
    <meta http-equiv="X-UA-Compatible" content="IE=edge">
    <meta name="viewport" content="width=device-width, initial-scale=1">
    <link rel="stylesheet" href="styles.css">
+   <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
    <script src="http://code.jquery.com/jquery-latest.min.js" type="text/javascript"></script>
    <script src="script.js"></script>
    <title>AAPMMS - DLSUD</title>
@@ -22,12 +23,18 @@
 </ul>
 </div>
 
-   
+<div class="w3-container w3-teal">
+   <h1>Incoming Air PM Sensor</h1>
+   </div>
 <div style = "position: relative; top: 10px; text-align: center">
 <iframe width="340" height="196" style="max-width: 100%; display: inline-block; border: 2px solid #00008b;" src="https://thingspeak.com/channels/743613/charts/1?bgcolor=%23ffffff&color=%23d62020&dynamic=true&api_key=C3JY2CR1I5TT22F1&results=30&type=line&update=15&height=196&width=340"></iframe>
 
 <iframe width="340" height="196" style="max-width: 100%; display: inline-block; border: 2px solid #00008b;" src="https://thingspeak.com/channels/743613/charts/2?bgcolor=%23ffffff&color=%23d62020&dynamic=true&api_key=C3JY2CR1I5TT22F1&results=30&type=line&update=15&height=196&width=340"></iframe>
 </div>
+   
+   <div class="w3-container w3-teal">
+   <h1>Outgoing Air PM Sensor</h1>
+   </div>
    
 <div style = "position: relative; top: 10px; text-align: center">
 <iframe width="340" height="196" style="max-width: 100%; display: inline-block; border: 2px solid #00008b;" src="https://thingspeak.com/channels/754899/charts/1?bgcolor=%23ffffff&color=%23d62020&dynamic=true&api_key=GVG0HRXKSNZ1IEFH&results=30&type=line&update=15&height=196&width=340"></iframe>
