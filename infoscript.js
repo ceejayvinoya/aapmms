9b1910e28c6a378310bdc12d6c1b371f29d735a1
ceejayvinoya <49735827+ceejayvinoya@users.noreply.github.com>
2019-04-22 03:03:19 +0800

    Add files via upload

---
+++ infoscript.js
@@ -0,0 +1,32 @@
+    $(document).ready(function(){
+    	$('#submit').on('click', function(){
+            $("#result").html("");
+    		if($('#time').val() == ""){			
+    			var warning = $("<div>Please complete the required field!</div>");
+    			$('#result').append(warning);
+    			setTimeout(function(){
+    				warning.fadeOut(1000);
+    			}, 2000);
+    		}else{
+    			$.ajax({
+                    
+    				type: 'POST',
+    				url: 'infotable.php',
+    				data: {
+    					//age:$('#age').val(),
+    					time:$('#time').val()
+    				},
+                    dataType: 'text',
+    				success: function(data){
+    					$('#result').append(data);
+                        //alert(data);
+    					
+    					
+    				},
+                    error: function() {
+                        alert('Not OKay');
+                    }
+    			});
+    		}
+    	});
+    });
