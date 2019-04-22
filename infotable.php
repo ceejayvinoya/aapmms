5a4dc80926ea4df301bafe64739d35707de67c98
ceejayvinoya <49735827+ceejayvinoya@users.noreply.github.com>
2019-04-22 03:02:53 +0000

    Update infotable.php

--- infotable.php
+++ infotable.php
@@ -17,15 +17,15 @@
 
     	$query = mysqli_query($conn, "SELECT * FROM insensor WHERE time LIKE '$time:%' AND date LIKE '$date'") or die(mysqli_error());
     	$row = mysqli_num_rows($query);
-        echo "<table style='width: 320px' class='w3-table-all'><tr class='w3-green'><th>PM10</th><th>PM2.5</th><th>Time</th></tr>";
+        echo "<table style='width: 320px' class='w3-table-all'><tr class='w3-green'><th>PM10</th><th>PM2.5</th><th>Time</th><th>Date</th></tr>";
         if($query->num_rows>0){    	
             while($row=$query->fetch_assoc()) {
-            echo "<tr class='w3-teal'><td>" . $row["apm10"]. "</td><td>" . $row["apm25"]. "</td><td>" . $row["time"]."</td></tr>";
+            echo "<tr class='w3-teal'><td>" . $row["apm10"]. "</td><td>" . $row["apm25"]. "</td><td>" . $row["time"]. "</td><td>" . $row["date"]. "</td></tr>";
         }
         echo "</table>";
         //if($row > 0){
     		//echo "success";
     	}else{
-    		echo "error";
+    		echo "<div class='w3-teal'><p>No data available for that time/date</p><div>";
     	}
     ?>
