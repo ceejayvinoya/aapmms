2012332be37094c4b102db9350461250cfaace1c
ceejayvinoya <49735827+ceejayvinoya@users.noreply.github.com>
2019-04-22 23:38:56 -0700

    Add files via upload

---
+++ inforefresh.php
@@ -0,0 +1,21 @@
+<?php
+    		$conn = mysqli_connect("ctgplw90pifdso61.cbetxkdyhwsb.us-east-1.rds.amazonaws.com", "yfxtv65r19dk2qm0", "rkiww4updt427u90", "hu1al3cymer6fwfu");
+	if(!$conn){
+		die("Error: Failed to connect to database!");
+	}
+     
+    	
+    	$query = mysqli_query($conn, "SELECT * FROM insensor ORDER BY entry_id DESC LIMIT 30") or die(mysqli_error());
+    	$row = mysqli_num_rows($query);
+        echo "<table style='width: 320px' class='w3-table-all'><tr class='w3-green'><th>PM10</th><th>PM2.5</th><th>Time</th><th>Date</th></tr>";
+        if($query->num_rows>0){    	
+            while($row=$query->fetch_assoc()) {
+            echo "<tr class='w3-teal'><td>" . $row["apm10"]. "</td><td>" . $row["apm25"]. "</td><td>" . $row["time"]. "</td><td>" . $row["date"]. "</td></tr>";
+        }
+        echo "</table>";
+        //if($row > 0){
+    		//echo "success";
+    	}else{
+    		echo "<div class='w3-teal'><p>No data available for that time/date</p><div>";
+    	}
+    ?>
