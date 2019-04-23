f19df1806212090d724fdd265995b267c597b0ef
ceejayvinoya <49735827+ceejayvinoya@users.noreply.github.com>
2019-04-24 02:16:57 +0800

    Update outfo.php

--- outfo.php
+++ outfo.php
@@ -44,7 +44,7 @@
 </div>
       
 <div id="result" class="w3-container w3-teal" style = "position: relative; top: 10px;">
-<table style="width: 320px" class="w3-table-all" style = "position: relative; top: 10px;">
+<table style="width: 320px; position: relative; top: 10px;" class="w3-table-all">
 <thead>
 <tr class="w3-green">
 <th>PM10</th> 
