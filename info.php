96cd3d1dc519f335510b517b22f4e5904e7d3b7c
ceejayvinoya <49735827+ceejayvinoya@users.noreply.github.com>
2019-04-26 07:43:29 +0800

    Update info.php

--- info.php
+++ info.php
@@ -19,8 +19,8 @@
    <li class='active'><a href='#'><span>Incoming Air</span></a></li>
    <li><a href='outfo.php'><span>Outgoing Air</span></a></li>
    <li><a href='status.php'><span>Status Update</span></a></li>
-   <li class='last'><a href='#', onClick="alert('One of the world’s leading problem is air pollution and there are many forms of pollutant that contribute in air pollution. Particulate matter which is the sum of all solid and liquid particles suspended in air many of which are hazardous. This complex mixture includes both organic and inorganic particles, such as dust, pollen, soot, smoke, and liquid droplets.This website monitors the daily value of particulate matter and with the hardware installed along Aguinaldo Highway, users can see progress of the mitigation through this website.')"><span>About</span></a></li>
-</ul>
+   <li class='last'><a href='about.html'><span>About</span></a></li>
+   </ul>
 </div>
 <div class="w3-container w3-teal">
    <h2>Incoming Air PM Data</h2>
