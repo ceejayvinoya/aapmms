57d9cd8176b9521a10c7593abd057e9daa6dfaa5
ceejayvinoya <49735827+ceejayvinoya@users.noreply.github.com>
2019-04-29 17:49:15 +0800

    Update info.php

--- info.php
+++ info.php
@@ -48,7 +48,7 @@
 
       
 <div id="result" class="w3-container">
-<table style="width: 320px; position: relative; top: 20px;" class="w3-table-all" >
+<table style="width: 320px; position: absolute; top: 20px;" class="w3-table-all" >
 <thead>
 <tr class="w3-green">
 <th>PM10</th> 
@@ -139,7 +139,7 @@
    
        
 <div id="result" class="w3-container">
-<table style="width: 320px; position: relative; top: 20px;" class="w3-table-all" >
+<table style="width: 320px; position: absolute; left: 330px; top: 20px;" class="w3-table-all" >
 <thead>
 <tr class="w3-green">
 <th>PM10</th> 
