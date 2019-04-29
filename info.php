ae47cfcd6631f9e924b8aaa19342d0f01cb20bb2
ceejayvinoya <49735827+ceejayvinoya@users.noreply.github.com>
2019-04-29 17:46:38 +0800

    Update info.php

--- info.php
+++ info.php
@@ -136,6 +136,97 @@
     $conn->close();
     ?>
 </div>
+   
+       
+<div id="result" class="w3-container">
+<table style="width: 320px; position: relative; top: 20px;" class="w3-table-all" >
+<thead>
+<tr class="w3-green">
+<th>PM10</th> 
+<th>PM2.5</th>
+<th>Time</th>
+<th>Date</th>
+</tr>    
+</thead>
+<?php
+
+    //LOGIN TO MYSQL DATABASE
+
+    $servername = "ctgplw90pifdso61.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
+    $username = "yfxtv65r19dk2qm0";
+    $password = "rkiww4updt427u90";
+    $database = "hu1al3cymer6fwfu";
+
+    $conn = new mysqli($servername, $username, $password, $database);
+
+    if($conn->connect_error){
+        die("</table> Connection Failed: " . $conn->connect_error);
+    }
+
+    //OBTAIN SENSOR DATA FROM THINGSPEAK
+
+    $jsondata = file_get_contents("https://api.thingspeak.com/channels/743613/feeds.json?api_key=R63HB8RHEB1IUOZF&timezone=Asia/Hong_Kong&results=30");
+    $json = json_decode($jsondata, true);
+    
+    $test = $json['feeds'][0]['entry_id'];
+    $res = $test;
+    $page = 'https://api.thingspeak.com/channels/743613/feeds.json?api_key=R63HB8RHEB1IUOZF&timezone=Asia/Hong_Kong&results=';
+    $jsondata = file_get_contents($page.$res);
+    $json = json_decode($jsondata, true);
+   
+   
+    //DECODE AND SORT DATA INTO SENSOR VALUES AND TIMESTAMPS
+
+    for($x = 0; $x < $res; $x++) {
+        $apm10 = $json['feeds'][$x]['field1'];
+        $apm25 = $json['feeds'][$x]['field2'];
+        $entry_id = $json['feeds'][$x]['entry_id'];
+
+        $timestamp = $json['feeds'][$x]['created_at'];
+
+        $divider = explode("T",$timestamp);
+        $date = $divider[0];
+        $timeset = $divider[1];
+
+        $timefix = explode("+",$timeset);
+        $time = $timefix[0];
+
+        //CHECK IF LATEST DATA IS ALREADY ON DATABASE
+
+        $sql = "SELECT * FROM insensor WHERE time = '$time' AND date = '$date'";
+        $result=$conn->query($sql);
+        if($result->num_rows>0){
+        
+        //DO NOTHING IF THE DATABASE IS ALREADY UPDATED
+
+        } else {
+            //ELSE IF DATA IS NOT UPDATED, INSERT UPDATED DATA TO DATABASE
+          
+                $sql="INSERT INTO insensor (apm10, apm25, time, date, entry_id)
+                VALUES
+                ('$apm10','$apm25','$time','$date','$entry_id')";
+    
+                if($conn->query($sql)!==TRUE){
+                    echo "</table> Error: " . $sql . "<br>" . $conn->error;
+                }
+            
+        }
+    }
+
+    //DISPLAY TABLE
+
+    $sql="SELECT * FROM insensor ORDER BY entry_id DESC LIMIT 30";
+    $result=$conn->query($sql);
+    if($result->num_rows>0){
+        while($row=$result->fetch_assoc()) {
+            echo "<tr class='w3-teal'><td>" . $row["apm10"]. "</td><td>" . $row["apm25"]. "</td><td>" . $row["time"]. "</td><td>" . $row["date"]."</td></tr>";
+        }
+        echo "</table>";
+    } else { echo "0 results"; }
+
+    $conn->close();
+    ?>
+</div>
   
 </body>
 </html>
