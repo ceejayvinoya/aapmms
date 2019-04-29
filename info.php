e58751039ec9e043799700266d787c08b98cd0b2
ceejayvinoya <49735827+ceejayvinoya@users.noreply.github.com>
2019-04-29 17:37:55 +0800

    Update info.php

--- info.php
+++ info.php
@@ -42,7 +42,7 @@
 
 </form>
    
-   <form method="get" class="w3-container" style="position: absolute">
+   <form method="get" class="w3-container" style="position: absolute; left: 160px">
    <button class="w3-btn w3-aqua" type="button" id="refresh">Refresh</button>
    </form>
 
