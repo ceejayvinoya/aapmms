b4487f21f708f46dc1ff7d9aa56bf64934143324
ceejayvinoya <49735827+ceejayvinoya@users.noreply.github.com>
2019-04-30 05:13:25 +0800

    Update info.php

--- info.php
+++ info.php
@@ -134,7 +134,7 @@
     $conn->close();
     ?>
 
-<table style="width: 320px; position: absolute; left: 330px" class="w3-table-all" >
+<table style="width: 320px; position: absolute; display: inline-block" class="w3-table-all" >
 <thead>
 <tr class="w3-green">
 <th>PM10</th> 
