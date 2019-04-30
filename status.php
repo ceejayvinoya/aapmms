ffc3a8b990a35d503e47afe64170e60c412b1a53
ceejayvinoya <49735827+ceejayvinoya@users.noreply.github.com>
2019-04-30 10:29:03 +0800

    Update status.php

--- status.php
+++ status.php
@@ -63,6 +63,22 @@
     } else {
         echo '<tr class="w3-teal"><td>Sensor 2</td><td>Inactive</td></tr>';
     }
+   $jsondata = file_get_contents("https://api.thingspeak.com/channels/769993/feeds.json?api_key=4LWAS8YZEWE9FFB8&results=1");
+    $json = json_decode($jsondata, true);
+    $timestamp = $json['feeds'][0]['created_at'];
+    $divider = explode("T",$timestamp);
+    $date = $divider[0];
+    $timeset = $divider[1];
+    $timefix = explode("+",$timeset);
+    $time = $timefix[0];
+    $B = date("Y-m-d h:i:s");
+    $timeA = strtotime($date.' '.$time);
+    $timeB = strtotime($B);
+    if(300 > $timeB - $timeA){
+        echo '<tr class="w3-teal"><td>Sensor 2</td><td>Running</td></tr>';
+    } else {
+        echo '<tr class="w3-teal"><td>Sensor 2</td><td>Inactive</td></tr>';
+    }
 ?>
 </table>
 </div>
