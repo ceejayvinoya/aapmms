4beaaa11e8514b4e9a26813b55438a050c3c08ee
ceejayvinoya <49735827+ceejayvinoya@users.noreply.github.com>
2019-05-26 22:59:33 +0800

    Update status.php

--- status.php
+++ status.php
@@ -31,22 +31,7 @@
     <td>Status</td>
   </tr>
 <?php
-    $jsondata = file_get_contents("https://api.thingspeak.com/channels/743613/feeds.json?api_key=R63HB8RHEB1IUOZF&results=1");
-    $json = json_decode($jsondata, true);
-    $timestamp = $json['feeds'][0]['created_at'];
-    $divider = explode("T",$timestamp);
-    $date = $divider[0];
-    $timeset = $divider[1];
-    $timefix = explode("+",$timeset);
-    $time = $timefix[0];
-    $B = date("Y-m-d h:i:s");
-    $timeA = strtotime($date.' '.$time);
-    $timeB = strtotime($B);
-    if(300 > $timeB - $timeA){
-        echo '<tr class="w3-teal"><td>Sensor 1</td><td>Running</td></tr>';
-    } else {
-        echo '<tr class="w3-teal"><td>Sensor 1</td><td>Inactive</td></tr>';
-    }
+    
      $jsondata = file_get_contents("https://api.thingspeak.com/channels/754899/feeds.json?api_key=PJ2C7CICC344DUVR&results=1");
     $json = json_decode($jsondata, true);
     $timestamp = $json['feeds'][0]['created_at'];
