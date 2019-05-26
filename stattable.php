c78967d83b9d486ab77d6fb2d1c9bcc1cba410c6
ceejayvinoya <49735827+ceejayvinoya@users.noreply.github.com>
2019-05-26 22:57:26 +0800

    Update stattable.php

--- stattable.php
+++ stattable.php
@@ -3,22 +3,7 @@ echo '<table style="width: 320px" class="w3-table-all">';
 echo '<tr class="w3-green">';
 echo '<td>Hardware</td>';
 echo '<td>Status</td></tr>';
-    $jsondata = file_get_contents("https://api.thingspeak.com/channels/743613/feeds.json?api_key=R63HB8RHEB1IUOZF&results=1");
-    $json = json_decode($jsondata, true);
-    $timestamp = $json['feeds'][0]['created_at'];
-    $divider = explode("T",$timestamp);
-    $date = $divider[0];
-    $timeset = $divider[1];
-    $timefix = explode("+",$timeset);
-    $time = $timefix[0];
-    $B = date("Y-m-d h:i:s");
-    $timeA = strtotime($date.' '.$time);
-    $timeB = strtotime($B);
-    if(300 > $timeB - $timeA){
-        echo '<tr class="w3-teal"><td>Sensor 1</td><td>Running</td></tr>';
-    } else {
-        echo '<tr class="w3-teal"><td>Sensor 1</td><td>Inactive</td></tr>';
-    }
+    
      $jsondata = file_get_contents("https://api.thingspeak.com/channels/754899/feeds.json?api_key=PJ2C7CICC344DUVR&results=1");
     $json = json_decode($jsondata, true);
     $timestamp = $json['feeds'][0]['created_at'];
