3428be775ddf09b4ecdac0df08c671b00e603b9c
ceejayvinoya <49735827+ceejayvinoya@users.noreply.github.com>
2019-10-23 16:30:35 +0800

    Update esp-data.php

--- esp-data.php
+++ esp-data.php
@@ -21,14 +21,12 @@ $sql = "SELECT id, apm25, time FROM insensor ORDER BY id DESC";
 
 echo '<table cellspacing="5" cellpadding="5">
       <tr> 
-        <td>ID</td> 
         <td>Value 1</td> 
         <td>Time</td> 
       </tr>';
  
 if ($result = $conn->query($sql)) {
     while ($row = $result->fetch_assoc()) {
-        $row_id = $row["id"];
         $row_apm25 = $row["apm25"];
         $row_time = $row["time"]; 
         
@@ -39,7 +37,6 @@ if ($result = $conn->query($sql)) {
         //$row_reading_time = date("Y-m-d H:i:s", strtotime("$row_reading_time + 4 hours"));
       
         echo '<tr> 
-                <td>' . $row_id . '</td> 
                 <td>' . $row_apm25 . '</td> 
                 <td>' . $row_time . '</td>
               </tr>';
