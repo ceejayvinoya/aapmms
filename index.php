5711783dea96ef826abafd4ad7c47197167fc74e
ceejayvinoya <49735827+ceejayvinoya@users.noreply.github.com>
2019-10-28 00:01:53 +0800

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,6 @@
 <head>
    <meta charset='utf-8'>
    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-   <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
    <meta name="viewport" content="width=device-width, initial-scale=1">
    <link rel="stylesheet" href="styles.css">
    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
