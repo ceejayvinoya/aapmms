022824c631baab11b67f89e38a9837bbb55c800c
ceejayvinoya <49735827+ceejayvinoya@users.noreply.github.com>
2019-10-27 20:51:14 +0800

    Update app.js

--- app.js
+++ app.js
@@ -17,19 +17,38 @@ $(document).ready(function(){
         datasets : [
           {
             label: 'value',
+            fill: false,
             backgroundColor: 'rgba(243, 18, 156 , 1)',
             borderColor: 'rgba(243, 18, 156 , 1)',
-            
-            data: value
+            data: value,
           }
-        ]
+        ],
       };
 
       var ctx = $("#mycanvas");
 
       var barGraph = new Chart(ctx, {
         type: 'line',
-        data: chartdata
+        data: chartdata,
+        options: {
+            title: {
+                    display: true,
+                    text: "Incoming Air"
+                },
+            maintainAspectRatio: false,
+            elements: {
+            line: {
+                    tension: 0.5 //Smoothening (Curved) of data lines
+                }
+            },
+            scales: {
+                    yAxes: [{
+                        ticks: {
+                            beginAtZero:true
+                        }
+                    }]
+            }
+        }
       });
     },
     error: function(data) {
