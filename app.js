c7a984e4331c602cdabd15526534d29f7e1ba79e
ceejayvinoya <49735827+ceejayvinoya@users.noreply.github.com>
2019-10-27 19:45:34 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,40 @@
+$(document).ready(function(){
+  $.ajax({
+    url: "http://www.aapmms.me/chart_data.php",
+    method: "GET",
+    success: function(data) {
+      console.log(data);
+      var value = [];
+      var time = [];
 
+      for(var i in data) {
+        player.push("Player " + data[i].value);
+        score.push(data[i].time);
+      }
+
+      var chartdata = {
+        labels: value,
+        datasets : [
+          {
+            label: 'value',
+            backgroundColor: 'rgba(200, 200, 200, 0.75)',
+            borderColor: 'rgba(200, 200, 200, 0.75)',
+            hoverBackgroundColor: 'rgba(200, 200, 200, 1)',
+            hoverBorderColor: 'rgba(200, 200, 200, 1)',
+            data: time
+          }
+        ]
+      };
+
+      var ctx = $("#mycanvas");
+
+      var barGraph = new Chart(ctx, {
+        type: 'line',
+        data: chartdata
+      });
+    },
+    error: function(data) {
+      console.log(data);
+    }
+  });
+});
