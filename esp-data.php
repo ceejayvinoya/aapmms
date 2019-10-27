118b151fe3c61b128482da348109b28a3e6641b6
ceejayvinoya <49735827+ceejayvinoya@users.noreply.github.com>
2019-10-27 17:17:29 +0800

    Update esp-data.php

--- esp-data.php
+++ esp-data.php
@@ -21,20 +21,14 @@ $sql = "SELECT id, apm25, time FROM insensor ORDER BY id DESC";
 
 echo '<table cellspacing="5" cellpadding="5">
       <tr> 
-        <td>Value 1</td> 
+        <td>Value 1 (ug/m3)</td> 
         <td>Time</td> 
       </tr>';
  
 if ($result = $conn->query($sql)) {
     while ($row = $result->fetch_assoc()) {
         $row_apm25 = $row["apm25"];
-        $row_time = $row["time"]; 
-        
-        // Uncomment to set timezone to - 1 hour (you can change 1 to any number)
-        //$row_reading_time = date("Y-m-d H:i:s", strtotime("$row_reading_time - 1 hours"));
-      
-        // Uncomment to set timezone to + 4 hours (you can change 4 to any number)
-        //$row_reading_time = date("Y-m-d H:i:s", strtotime("$row_reading_time + 4 hours"));
+        $row_time = $row["time"];
       
         echo '<tr> 
                 <td>' . $row_apm25 . '</td> 
