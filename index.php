214ea2e8901471bd65b274d3965f8b85a41058ef
ceejayvinoya <49735827+ceejayvinoya@users.noreply.github.com>
2019-11-02 18:44:11 +0800

    Update index.php

--- index.php
+++ index.php
@@ -6,6 +6,9 @@
    <meta name="viewport" content="width=device-width, initial-scale=1">
    <link rel="stylesheet" href="styles.css">
    <link rel="stylesheet" href="w3.css">
+   <script type="text/javascript" src="jquery.min.js"></script>
+   <script type="text/javascript" src="chart.min.js"></script>
+   <script type="text/javascript" src="app.js"></script>
    <title>AAPMMS - DLSUD</title>
    <style type="text/css">
       #chart-container {
@@ -33,9 +36,6 @@
 <div id="chart-container" class="w3-container" style = "position: relative">
    <canvas id="mycanvas"></canvas>
    </div>
-   <script type="text/javascript" src="jquery.min.js"></script>
-    <script type="text/javascript" src="chart.min.js"></script>
-<script type="text/javascript" src="app.js"></script>
-
+   
 </body>
 </html>
