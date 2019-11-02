679af1dc117e3e6d2efff427d9486afa2fc8e152
ceejayvinoya <49735827+ceejayvinoya@users.noreply.github.com>
2019-11-02 17:57:23 +0800

    Update table.js

--- table.js
+++ table.js
@@ -1,34 +1,41 @@
-var myList=[{"name" : "abc", "age" : 50},
-            {"age" : "25", "hobby" : "swimming"},
-            {"name" : "xyz", "hobby" : "programming"}];
+$(document).ready(function(){
+    $.ajax({
+        url: "http://www.aapmms.me/chart_data.php",
+        method: "GET",
+        success: function(data) {
+            console.log(data);
+            var value = [];
+            var time = [];
 
-// Builds the HTML Table out of myList json data from Ivy restful service.
- function buildHtmlTable() {
-     var columns = addAllColumnHeaders(myList);
- 
-     for (var i = 0 ; i < myList.length ; i++) {
-         var row$ = $('<tr/>');
-         for (var colIndex = 0 ; colIndex < columns.length ; colIndex++) {
-             var cellValue = myList[i][columns[colIndex]];
- 
-             if (cellValue == null) { cellValue = ""; }
- 
-             row$.append($('<td/>').html(cellValue));
-         }
-         $("#excelDataTable").append(row$);
-     }
- }
- 
- // Adds a header row to the table and returns the set of columns.
- // Need to do union of keys from all records as some records may not contain
- // all records
- function addAllColumnHeaders(myList)
+            for(var i in data) {
+                value.push(data[i].value);
+                time.push(data[i].timestamp);
+            }
+
+            var columns = addAllColumnHeaders(data);
+
+            for (var i = 0 ; i < data.length ; i++) {
+                var row$ = $('<tr/>');
+                for (var colIndex = 0 ; colIndex < columns.length ; colIndex++) {
+                    var cellValue = data[i][columns[colIndex]];
+        
+                    if (cellValue == null) { cellValue = ""; }
+        
+                    row$.append($('<td/>').html(cellValue));
+                }
+                $("#excelDataTable").append(row$);
+       }
+        },
+    });
+});
+
+function addAllColumnHeaders(data)
  {
      var columnSet = [];
      var headerTr$ = $('<tr/>');
  
-     for (var i = 0 ; i < myList.length ; i++) {
-         var rowHash = myList[i];
+     for (var i = 0 ; i < data.length ; i++) {
+         var rowHash = data[i];
          for (var key in rowHash) {
              if ($.inArray(key, columnSet) == -1){
                  columnSet.push(key);
@@ -38,5 +45,5 @@ var myList=[{"name" : "abc", "age" : 50},
      }
      $("#excelDataTable").append(headerTr$);
  
-     return columnSet;
+return columnSet;
  }
