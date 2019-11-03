cb32690a39713c81bbf6106b79865b42ffb2d029
ceejayvinoya <49735827+ceejayvinoya@users.noreply.github.com>
2019-11-03 09:59:02 +0800

    Update info.php

--- info.php
+++ info.php
@@ -1,3 +1,4 @@
+
 <!doctype html>
 <html lang=''>
 <head>
@@ -6,29 +7,38 @@
    <meta name="viewport" content="width=device-width, initial-scale=1">
    <link rel="stylesheet" href="styles.css">
    <link rel="stylesheet" href="w3.css">
-   <script type="text/javascript" src="table.js"></script>
+   <script type="text/javascript" src="jquery.min.js"></script>
    <script type="text/javascript" src="chart.min.js"></script>
-   <script src="jquery.min.js" type="text/javascript"></script>
+   <script type="text/javascript" src="app.js"></script>
+   <script type="text/javascript" src="table.js"></script>
    <title>AAPMMS - DLSUD</title>
+   <style type="text/css">
+      #chart-container {
+        width: 60%;
+        height: 360px;
+        margin: 0 auto;
+        float: left;
+      }
+      th {
+        font-weight : bold
+      }
+    </style>
 </head>
 <body onLoad="buildHtmlTable()">
-
 <div id='cssmenu'>
 <ul>
-   <li><a href='index.php'><span>Home</span></a></li>
+   <li><a href='#'><span>Home</span></a></li>
    <li class='active'><a href='#'><span>Tables</span></a></li>
    <li class='last'><a href='about.html'><span>About</span></a></li>
-   </ul>
+</ul>
 </div>
-   
-<div class="w3-container">
-   <h2>Air PM Data</h2>
+<div class="w3-container w3-lime">
+   <h1>Automated Air Pollution Monitoring and Mitigation System</h1>
    </div>
    
-   <div>
-   <table id="excelDataTable" border="1">
-</table>
+   <div class="w3-container">
+      <table id="excelDataTable" border="1">
+      </table>
    </div>
-      
 </body>
 </html>
