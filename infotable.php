403a8b5825f945a17353f20d772f43754dd9694a
ceejayvinoya <49735827+ceejayvinoya@users.noreply.github.com>
2019-11-08 20:09:59 +0800

    Update infotable.php

--- infotable.php
+++ infotable.php
@@ -10,7 +10,7 @@
 
 	$query = mysqli_query($conn, "SELECT * FROM insensor WHERE time LIKE '$time%' ORDER BY id DESC") or die(mysqli_error());
     	$row = mysqli_num_rows($query);
-        echo "<table width='320' style='max-width: 320px; display: inline-table; border: 2px solid #00008b' class='w3-table-all'><tr class='w3-green'><th>PM10</th><th>PM2.5</th><th>Time</th><th>Date</th></tr>";
+        echo "<table width='320' style='max-width: 320px; display: inline-table; border: 2px solid #00008b' class='w3-table-all'><tr class='w3-green'><th>PM10</th><th>PM2.5</th><th>Time</th></tr>";
         if($query->num_rows>0){    	
             while($row=$query->fetch_assoc()) {
             echo "<tr class='w3-teal'><td>" . $row["apm25"]. "</td><td>" . $row["bpm25"]. "</td><td>" . $row["time"]. "</td></tr>";
