3a5e380b9496a1036824d1c49508c6831b761743
ceejayvinoya <49735827+ceejayvinoya@users.noreply.github.com>
2019-11-08 16:52:07 +0800

    Update infotable.php

--- infotable.php
+++ infotable.php
@@ -6,21 +6,14 @@
 		die("Error: Failed to connect to database!");
 	}
      
-    	$time = $_POST["time"];
     	$date = $_POST["date"];
-   
-        $num = strlen((string)$time);
 
-        if($num == 1){
-          $time = "0".$time;
-        }
-
-	$query = mysqli_query($conn, "SELECT * FROM insensortwo WHERE time LIKE '$time:%' AND date LIKE '$date' ORDER BY date DESC, time DESC") or die(mysqli_error());
+	$query = mysqli_query($conn, "SELECT * FROM insensor WHERE  date LIKE '$date%' ORDER BY id DESC") or die(mysqli_error());
     	$row = mysqli_num_rows($query);
         echo "<table width='320' style='max-width: 320px; display: inline-table; border: 2px solid #00008b' class='w3-table-all'><tr class='w3-green'><th>PM10</th><th>PM2.5</th><th>Time</th><th>Date</th></tr>";
         if($query->num_rows>0){    	
             while($row=$query->fetch_assoc()) {
-            echo "<tr class='w3-teal'><td>" . $row["cpm10"]. "</td><td>" . $row["cpm25"]. "</td><td>" . $row["time"]. "</td><td>" . $row["date"]. "</td></tr>";
+            echo "<tr class='w3-teal'><td>" . $row["apm25"]. "</td><td>" . $row["bpm25"]. "</td><td>" . $row["time"]. "</td></tr>";
         }
         echo "</table>";
         //if($row > 0){
