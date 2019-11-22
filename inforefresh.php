d429d0feb394c4cb95e84667469baa7193209df3
ceejayvinoya <49735827+ceejayvinoya@users.noreply.github.com>
2019-11-22 04:46:05 +0000

    Update inforefresh.php

--- inforefresh.php
+++ inforefresh.php
@@ -9,14 +9,14 @@
             die("</table> Connection Failed: " . $conn->connect_error);
          }
     //DISPLAY TABLE
-        echo "<table style='position: relative; top: 10px; border: 2px solid #00008b' class='w3-table-all'>" 
-        echo "<thead>" 
-        echo "<tr class='w3-green'>" 
-        echo "<th>Incoming (ug/m3)</th>" 
-        echo "<th>Outgoing (ug/m3)</th>" 
-        echo "<th>Time</th>" 
-        echo "</tr>" 
-        echo "</thead>" 
+        echo "<table style='position: relative; top: 10px; border: 2px solid #00008b' class='w3-table-all'>";
+        echo "<thead>";
+        echo "<tr class='w3-green'>";
+        echo "<th>Incoming (ug/m3)</th>";
+        echo "<th>Outgoing (ug/m3)</th>";
+        echo "<th>Time</th>";
+        echo "</tr>";
+        echo "</thead>";
          $sql="SELECT * FROM insensor ORDER BY id DESC LIMIT 40";
          $result=$conn->query($sql);
          if($result->num_rows>0){
